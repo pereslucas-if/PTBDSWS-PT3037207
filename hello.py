9b2062bf6448aca33fe9cc99bcb2b1b8634ab8a9
pereslucas-if <peres.lucas@aluno.ifsp.edu.br>
2026-09-20 20:02:40 +0000

    Envio de email

--- hello.py
+++ hello.py
@@ -1,56 +1,92 @@
 import os
-from flask import Flask, render_template, session, redirect, url_for
+import requests
+from threading import Thread
+from dotenv import load_dotenv
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
+# Carrega as variáveis do arquivo .env
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Variáveis do Mailgun e da aplicação (padrão dos slides do professor)
+app.config['API_KEY'] = os.environ.get('API_KEY')
+app.config['API_URL'] = os.environ.get('API_URL')
+app.config['API_FROM'] = os.environ.get('API_FROM')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+app.config['MY_PRIMARY_EMAIL'] = os.environ.get('MY_PRIMARY_EMAIL')
+app.config['MY_INSTITUTIONAL_EMAIL'] = os.environ.get('MY_INSTITUTIONAL_EMAIL')
+app.config['STUDENT_NAME'] = os.environ.get('STUDENT_NAME', 'Lucas Peres Gomes')
+app.config['STUDENT_ID'] = os.environ.get('STUDENT_ID', 'PT3037207')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    role = SelectField('Role?:', coerce=int)
     submit = SubmitField('Submit')
 
 
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+def send_async_email(app_context, recipients, subject, html_content):
+    """Executa o envio via API do Mailgun em background"""
+    with app_context:
+        api_key = app.config['API_KEY']
+        api_url = app.config['API_URL']
+        api_from = app.config['API_FROM']
+
+        for recipient in recipients:
+            if not recipient:
+                continue
+
+            try:
+                response = requests.post(
+                    api_url,
+                    auth=("api", api_key),
+                    data={
+                        "from": api_from,
+                        "to": [recipient],
+                        "subject": subject,
+                        "html": html_content
+                    }
+                )
+                print(f"[Mailgun] Envio para {recipient} - Status: {response.status_code}")
+            except Exception as e:
+                print(f"[Mailgun] Erro ao enviar para {recipient}: {e}")
+
+
+def send_email(subject, new_username):
+    """Monta o HTML diretamente no Python e dispara a Thread"""
+    recipients = list(set([
+        app.config['FLASKY_ADMIN'],
+        app.config['MY_INSTITUTIONAL_EMAIL'],
+        app.config['MY_PRIMARY_EMAIL']
+    ]))
+
+    full_subject = app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject
+
+    # HTML gerado diretamente sem precisar de ficheiro de template
+    html_content = f"""
+    <h3>Novo Usuário Cadastrado</h3>
+    <p><b>Prontuário do Aluno:</b> {app.config['STUDENT_ID']}</p>
+    <p><b>Nome do Aluno:</b> {app.config['STUDENT_NAME']}</p>
+    <p><b>Novo Usuário Cadastrado:</b> {new_username}</p>
+    """
+
+    thr = Thread(
+        target=send_async_email,
+        args=[app.app_context(), recipients, full_subject, html_content]
+    )
+    thr.start()
+    return thr
 
 
 @app.errorhandler(404)
@@ -66,51 +102,20 @@ def internal_server_error(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-
-    # Ordem personalizada para exibição das roles
-    desired_order = ['Administrator', 'Moderator', 'User']
-
-    # Busca todas as roles do banco e ordena na sequência exata desejada
-    roles_all = Role.query.all()
-    roles = sorted(roles_all, key=lambda r: desired_order.index(r.name) if r.name in desired_order else 99)
-
-    # Popula o SelectField com as escolhas ordenadas
-    form.role.choices = [(role.id, role.name) for role in roles]
-
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            selected_role = Role.query.get(form.role.data)
-            user = User(username=form.name.data, role=selected_role)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-        else:
-            session['known'] = True
+        old_name = session.get('name')
+        if old_name is None or old_name != form.name.data:
+            send_email(
+                subject='Novo usuário cadastrado',
+                new_username=form.name.data
+            )
+            flash('Um email foi enviado ao seu endereço.')
+
         session['name'] = form.name.data
         return redirect(url_for('index'))
 
-    # Consultas para as tabelas e contadores
-    users = User.query.all()
-    num_users = len(users)
-    num_roles = len(roles)
+    return render_template('index.html', form=form, name=session.get('name'))
 
-    return render_template('index.html',
-                           form=form,
-                           name=session.get('name'),
-                           known=session.get('known', False),
-                           users=users,
-                           roles=roles,
-                           num_users=num_users,
-                           num_roles=num_roles)
-
-with app.app_context():
-    db.create_all()
-    roles = ['Administrator', 'Moderator', 'User']
-    for r in roles:
-        if not Role.query.filter_by(name=r).first():
-            db.session.add(Role(name=r))
-    db.session.commit()
 
 if __name__ == '__main__':
     app.run(debug=True)
